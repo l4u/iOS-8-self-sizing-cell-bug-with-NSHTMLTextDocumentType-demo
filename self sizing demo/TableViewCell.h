5262cf0f38063ede407ed3778a2e2dd21a6c9891
Leo Lou <louyuhong@gmail.com>
2014-10-03 07:18:23 +0800

    Add TableViewCell

---
+++ self sizing demo/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  self sizing demo
+//
+//  Created by Leo on 10/3/14.
+//  Copyright (c) 2014 Cursorless Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ self sizing demo/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  self sizing demo
+//
+//  Created by Leo on 10/3/14.
+//  Copyright (c) 2014 Cursorless Limited. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
