dab8aef2a28f91b6174df7e2b41ef34923bfaf00
Leo Lou <louyuhong@gmail.com>
2014-10-03 07:15:41 +0800

    Initial Commit

---
+++ self sizing demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  self sizing demo
+//
+//  Created by Leo on 10/3/14.
+//  Copyright (c) 2014 Cursorless Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ self sizing demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  self sizing demo
+//
+//  Created by Leo on 10/3/14.
+//  Copyright (c) 2014 Cursorless Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
